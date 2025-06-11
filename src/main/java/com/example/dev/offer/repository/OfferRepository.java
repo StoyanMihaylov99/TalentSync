0bf435985246f0c4c6e25e8e9295132277159295
StoyanMihaylov99 <stoqn.business@gmail.com>
2025-06-11 13:56:46 +0300

    Implemented offer business logic.

--- src/main/java/com/example/dev/offer/repository/OfferRepository.java
+++ src/main/java/com/example/dev/offer/repository/OfferRepository.java
@@ -1,10 +1,18 @@
 package com.example.dev.offer.repository;
 
 import com.example.dev.offer.model.Offer;
+import io.micrometer.common.lang.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, String> {
+
+    @NonNull
+    Optional<Offer> findByTitle(@NonNull String title);
+
+    void deleteById(@NonNull String id);
 
 }
